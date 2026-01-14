64ff1c76a1bc78b23ac73acc14b89eca5bd1bbda
Arnaud Huge <ahuge@3E3.42angouleme.fr>
2026-01-14 14:38:08 +0100

    print type and bind of a symbol

--- src/nm.c
+++ src/nm.c
@@ -25,7 +25,7 @@ Elf64_Ehdr	*get_elf_hearder(int fd, int page_size)
 	return (ptr);
 }
 
-Elf64_Shdr	*get_section_header(int fd, Elf64_Ehdr *elfHeader, int page_size)
+Elf64_Shdr	*get_sections_header(int fd, Elf64_Ehdr *elfHeader, int page_size)
 {
 	int	aligned = elfHeader->e_shoff - (elfHeader->e_shoff % page_size);
 
@@ -47,7 +47,7 @@ Elf64_Shdr	*get_section_header(int fd, Elf64_Ehdr *elfHeader, int page_size)
 	return (ptr);
 }
 
-char	*get_mapped_and_copied(int fd, Elf64_Shdr *sectionNameHeader, int page_size)
+char	*get_section_by_header(int fd, Elf64_Shdr *sectionNameHeader, int page_size)
 {
 	int	aligned = sectionNameHeader->sh_offset - (sectionNameHeader->sh_offset % page_size);
 
@@ -86,7 +86,7 @@ char	*get_section_by_name(int fd, const char *str, Elf64_Shdr *sectionHeader, ch
 		printf("Section not found : %s\n", str);
 		return (NULL);
 	}
-	ptr = get_mapped_and_copied(fd, &sectionHeader[i], page_size);
+	ptr = get_section_by_header(fd, &sectionHeader[i], page_size);
 	return (ptr);
 }
 
@@ -109,6 +109,41 @@ Elf64_Shdr	*get_section_header_by_name(const char *str, Elf64_Shdr	*sectionHeade
 	return (&sectionHeader[i]);
 }
 
+void	print_symbol_type(Elf64_Sym *symtab)
+{
+	int	bind = ELF64_ST_BIND(symtab->st_info);
+	int	type = ELF64_ST_TYPE(symtab->st_info);
+	printf("bind ; %d  type; %d  sndx; %d  ", bind, type, symtab->st_shndx);
+}
+
+void print_symbol_line(Elf64_Sym *symtab, char *name)
+{
+	if (symtab->st_value)
+		printf("%016lx ", symtab->st_value);
+	else
+		printf("                 ");
+
+	print_symbol_type(symtab);
+	
+	printf("%s\n", name);
+}
+
+int	print_symbols(Elf64_Shdr *symtabHeader, Elf64_Sym *symtab, char *strtab, Elf64_Shdr *dynsymHeader, Elf64_Sym *dynsym, char *dynstr)
+{
+	for (int i = 0; i < symtabHeader->sh_size / symtabHeader->sh_entsize; i++)
+	{
+		print_symbol_line(&symtab[i], strtab + symtab[i].st_name);
+	}
+
+	printf("\n");
+
+	// for (int i = 0; i < dynsymHeader->sh_size / dynsymHeader->sh_entsize; i++)
+	// {
+	// 	printf("%lu  %s\n", dynsym[i].st_value, dynstr + dynsym[i].st_name);
+	// }
+	return (0);
+}
+
 int main(void)
 {
 	int fd = open("a.out", O_RDONLY);
@@ -121,35 +156,25 @@ int main(void)
 	if (!elfHeader)
 		return (1);
 
-	Elf64_Shdr	*sectionHeader = get_section_header(fd, elfHeader, page_size);
+	Elf64_Shdr	*sectionHeader = get_sections_header(fd, elfHeader, page_size);
 	if (!sectionHeader)
 		return (1);
 
-	char	*shstrtab = get_mapped_and_copied(fd, &sectionHeader[elfHeader->e_shstrndx], page_size);
-
-	Elf64_Sym	*symtab = (Elf64_Sym *)get_section_by_name(fd, "symtab", sectionHeader, shstrtab, page_size, elfHeader->e_shnum);
+	char	*shstrtab = get_section_by_header(fd, &sectionHeader[elfHeader->e_shstrndx], page_size);
 
 	Elf64_Shdr	*symtabHeader = get_section_header_by_name("symtab", sectionHeader, shstrtab, elfHeader->e_shnum);
 
-	Elf64_Sym	*dynsym = (Elf64_Sym *)get_section_by_name(fd, "dynsym", sectionHeader, shstrtab, page_size, elfHeader->e_shnum);
+	Elf64_Sym	*symtab = (Elf64_Sym *)get_section_by_header(fd, symtabHeader, page_size);
 
 	Elf64_Shdr	*dynsymHeader = get_section_header_by_name("dynsym", sectionHeader, shstrtab, elfHeader->e_shnum);
+
+	Elf64_Sym	*dynsym = (Elf64_Sym *)get_section_by_header(fd, dynsymHeader, page_size);
 
 	char	*strtab = get_section_by_name(fd, "strtab", sectionHeader, shstrtab, page_size, elfHeader->e_shnum);
 
 	char	*dynstr = get_section_by_name(fd, "dynstr", sectionHeader, shstrtab, page_size, elfHeader->e_shnum);
 
-	for (int i = 0; i < symtabHeader->sh_size / symtabHeader->sh_entsize; i++)
-	{
-		printf("%lx  %s\n", symtab->st_value, strtab + symtab[i].st_name);
-	}
-
-	printf("\n");
-
-	for (int i = 0; i < dynsymHeader->sh_size / dynsymHeader->sh_entsize; i++)
-	{
-		printf("%lu  %s\n", dynsym[i].st_value, dynstr + dynsym[i].st_name);
-	}
+	print_symbols(symtabHeader, symtab, strtab, dynsymHeader, dynsym, dynstr);
 
 	free(elfHeader);
 	free(sectionHeader);
